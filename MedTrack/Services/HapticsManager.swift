4d00e589a51784e0c4d066941ac903ec6561178e
this-isaryan <aryankumar11@outlook.com>
2025-08-16 15:41:25 -0400

    Modified the MdeicineDetailView page, added button animation and Haptic feedback

---
+++ MedTrack/Services/HapticsManager.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+enum HapticsManager {
+    static func impact(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+
+    static func notify(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+}
